967beebd58d840455ab9717581006b6dedc812d3
samimaktar2001 <samimaktar742166@gmail.com>
2026-08-28 13:13:47 +0530

    migration complete

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,11 +2,13 @@ import type { Config } from "drizzle-kit";
 import * as dotenv from "dotenv";
 
 dotenv.config({ path: ".env" });
+const dbUrl = (process.env.DIRECT_URL || process.env.NEON_DB || process.env.DATABASE_URL || "").replace(":6543/", ":5432/");
+
 export default {
   schema: "./src/db/schema.ts",
   out: "./src/db/migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.NEON_DB!,
+    url: dbUrl,
   },
 } satisfies Config;
